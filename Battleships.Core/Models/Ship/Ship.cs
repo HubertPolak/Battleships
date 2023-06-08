64aa389888e612dbf21f5c4ccc1f2d5967df3eb9
Anna Przybycien <ania.przybycien@gmail.com>
2023-06-08 18:33:07 +0200

    Add project files.

---
+++ Battleships.Core/Models/Ship/Ship.cs
@@ -0,0 +1,17 @@
+﻿namespace Battleships.Core.Models.Ship;
+
+public class Ship
+{
+    public int Length { get; }
+
+    private Ship(int length)
+    {
+        Length = length;
+    }
+
+    public static Ship Create(int length)
+    {
+        return new Ship(length);
+    }
+}
+
